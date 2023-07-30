69579306f2e3abd158d43184ff5893f6b45043a2
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2023-07-30 20:52:54 +0700

    Getting keyword_translation, english_example_list, russian_example_list from get_online_translator_data

--- app/main.py
+++ app/main.py
@@ -23,14 +23,60 @@ def get_cambridge_data(keyword):
 
 
 def get_online_translator_data(keyword):
+    keyword = keyword.lower()
     url = f"https://www.online-translator.com/translation/english-russian/{keyword}"
     html = requests.get(url, headers=headers, timeout=6)
 
     soup = BeautifulSoup(html.text, features="html.parser")
 
-    return soup
+    raw_keyword_translation = soup.find_all(
+        "span",
+        class_="result_only sayWord",
+        limit=3,
+    )
+    keyword_translation = []
+    for word in raw_keyword_translation:
+        keyword_translation.append(word.text)
+
+    raw_english_examples = soup.find(
+        "div",
+        id="topSamplesSelSource",
+    ).find_all(
+        "span",
+        class_="samSource",
+        limit=3,
+    )
+
+    english_example_list = []
+    for example in raw_english_examples:
+        example = str(example)[24:-7]
+        english_example_list.append(
+            example.replace('<span class="samSource">', "")
+            .replace('<span class="sourceSample">', "<b>")
+            .replace("</span>", "</b>")
+        )
+
+    raw_russian_examples = soup.find(
+        "div",
+        id="topSamplesSelSource",
+    ).find_all(
+        "span",
+        class_="samTranslation",
+        limit=3,
+    )
+
+    russian_example_list = []
+    for example in raw_russian_examples:
+        example = str(example)[29:-7]
+        russian_example_list.append(
+            example.replace('<span class="samTranslation">', "")
+            .replace('<span class="sourceSample">', "<b>")
+            .replace("</span>", "</b>")
+        )
+
+    return keyword_translation, english_example_list, russian_example_list
 
 
 if __name__ == "__main__":
-    get_cambridge_data(input().lower())
-    # print(get_online_translator_data(input().lower()))
+    # get_cambridge_data(input().lower())
+    print(get_online_translator_data(input().lower()))
