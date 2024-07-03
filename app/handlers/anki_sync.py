56eeaeb509a7620cce3718959cfce08e3d514423
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2024-07-03 12:06:24 +0700

    fix check user admin from handlers

--- app/handlers/anki_sync.py
+++ app/handlers/anki_sync.py
@@ -1,16 +1,26 @@
 from telegram import Update
 from telegram.ext import ContextTypes
 
+from app.__main__ import check_admin_user
 from app.anki_connect import get_sync, load_profile
 from app.messages import messages
 
 
 async def get_anki_sync(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    load_profile("User 1")
-    get_sync()  # TODO exceptions
+    bot_user = update.effective_user.id
 
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=messages.get("sync_success"),
-        parse_mode="html",
-    )
+    if check_admin_user(bot_user):
+        load_profile("User 1")
+        get_sync()  # TODO exceptions
+
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=messages.get("sync_success"),
+            parse_mode="html",
+        )
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=messages.get("private_bot"),
+            parse_mode="html",
+        )
--- app/handlers/start.py
+++ app/handlers/start.py
@@ -7,18 +7,23 @@ from app.messages import messages
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    db.create_bot_user_table()
-    db.create_keyword_data_table()
-
     bot_user = update.effective_user.id
 
-    if not db.check_user_exists(bot_user):
-        db.insert_user(bot_user)
+    if check_admin_user(bot_user):
+        db.create_bot_user_table()
+        db.create_keyword_data_table()
 
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text=messages.get("start")
-        if check_admin_user(bot_user)
-        else messages.get("private_bot"),
-        parse_mode="html",
-    )
+        if not db.check_user_exists(bot_user):
+            db.insert_user(bot_user)
+
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=messages.get("start"),
+            parse_mode="html",
+        )
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=messages.get("private_bot"),
+            parse_mode="html",
+        )
